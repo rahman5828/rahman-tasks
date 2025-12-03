905e024f6a050d9fb9c2233060000ff7b9d173c9
Abdul Rahman <abdulrahman6148@gmail.com>
2025-12-03 11:53:09 +0530

    feat: Day 01 Strapi project setup with content type and sample data

---
+++ src/api/devops-project/controllers/devops-project.ts
@@ -0,0 +1,7 @@
+/**
+ * devops-project controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::devops-project.devops-project');
---
+++ src/api/devops-project/routes/devops-project.ts
@@ -0,0 +1,7 @@
+/**
+ * devops-project router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::devops-project.devops-project');
