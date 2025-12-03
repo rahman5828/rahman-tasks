bf441f6ad262c37d87761c95466afd85dec7c54c
Abdul Rahman <abdulrahman6148@gmail.com>
2025-12-03 12:26:14 +0530

    chore: empty main branch for Day 01 PR

--- src/api/devops-project/controllers/devops-project.ts
+++
@@ -1,7 +0,0 @@
-/**
- * devops-project controller
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreController('api::devops-project.devops-project');
--- src/api/devops-project/routes/devops-project.ts
+++
@@ -1,7 +0,0 @@
-/**
- * devops-project router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::devops-project.devops-project');
